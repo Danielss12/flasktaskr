5315872078d83b7c32e16a3bb7cf05a153dc458d
Danilo <dlb__@hotmail.com>
2016-11-18 02:02:48 +0000

    changed the database from vanilla sql to orm SQLAlchemy

--- project/_config.py
+++ project/_config.py
@@ -9,3 +9,6 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = '\xd7\xad\xf1\xe7\xbe\xa2\xdb\xa5N`\x16\x14\xfc\x94\x999\xc5\xbdtr\xbf\xec\r\xd9'
 
 DATABASE_PATH =	os.path.join(basedir, DATABASE)
+
+#database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
--- project/db_create.py
+++ project/db_create.py
@@ -1,19 +1,15 @@
-import sqlite3
-from config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-	c = connection.cursor()
+#create the database and the table
+db.create_all()
 
-	c.execute("DROP TABLE if exists tasks")
+#insert data
 
-	#create table
-	c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL, status INTEGER NOT NULL)""")
+db.session.add(Task("Finish this tutorial", date(2015, 3, 13), 10, 1))
 
-	#insert dummy data into table
-	c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-		'VALUES("Finish this tutorial", "12/25/2016", 10, 1)')
+db.session.add(Task("Finish Real Python", date(2015, 3, 13), 10, 1))
 
-	c.execute('INSERT INTO tasks (name, due_date, priority, status)'
-		'VALUES("Finish Real Python Course 2", "12/09/2016", 10, 1)')
-
+#commit the changes
+db.session.commit()
