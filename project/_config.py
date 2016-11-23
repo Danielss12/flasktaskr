3ac39b5ec57a96b56e014c300f6f1d5e3179fde7
Danilo <dlb__@hotmail.com>
2016-11-23 23:50:40 +0000

    register user and login funtionality

--- project/_config.py
+++ project/_config.py
@@ -3,8 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE ='flasktaskr.db'
-USERNAME ='admin'
-PASSWORD ='admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '\xd7\xad\xf1\xe7\xbe\xa2\xdb\xa5N`\x16\x14\xfc\x94\x999\xc5\xbdtr\xbf\xec\r\xd9'
 
